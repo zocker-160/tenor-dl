c68ee8b4bd60c1a62ca0a1fb4cf19767b965f462
zocker-160 <zocker1600@posteo.net>
2022-08-04 23:35:59 +0200

    add -o and -u cli options because why not

--- README.md
+++ README.md
@@ -11,15 +11,18 @@ pip3 install tenor-dl
 ## Usage
 
 ```bash
-usage: tenor-dl [-h] URL
+usage: tenor-dl [-h] [-o OUTPUT] [-u] URL
 
 simple script to download gifs from tenor.com
 
 positional arguments:
-  URL         tenor.com URL to download the gif from
+  URL                   tenor.com URL to download the gif from
 
 optional arguments:
-  -h, --help  show this help message and exit
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        output path or use "-" for STDOUT
+  -u, --urlonly         only print direct link to gif and exit
 ```
 
 ## Example Usage
--- tenor_dl/tenor_dl.py
+++ tenor_dl/tenor_dl.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
 
+import os
 import json
 import requests
 import argparse
 
 from pyquery import PyQuery
 
-def downloadURL(url: str):
+def downloadURL(url: str, outLoc: str, urlOnly=False):
     #URL = "https://tenor.com/view/rage-work-pc-stressed-pissed-gif-15071896"
 
     print("fetching URL")
@@ -23,21 +24,31 @@ def downloadURL(url: str):
         return
 
     data = json.loads(jsonData.html())
+
     id = url.split("-")[-1]
-
     results = data["gifs"]["byId"][id]["results"][0]
-
     name = results["h1_title"]
     url = results["media"][0]["gif"]["url"]
 
     print("found gif:", name)
+
+    if urlOnly:
+        print("GIF direct link:", url)
+        return
+
     print("downloading gif...")
 
     gif = requests.get(url)
 
     print("writing file")
 
-    with open(name+".gif", "wb") as f:
+    if outLoc == "-":
+        print(gif.content)
+        return
+
+    outPath = os.path.join(outLoc, name+".gif")
+
+    with open(outPath, "wb") as f:
         f.write(gif.content)
 
     print("done")
@@ -49,9 +60,22 @@ def parseArgs():
 
     parser.add_argument(
         "URL", type=str, help="tenor.com URL to download the gif from")
+    parser.add_argument(
+        "-o", "--output", type=str, default=".", dest="output",
+        help="output path or use \"-\" for STDOUT"
+    )
+    parser.add_argument(
+        "-u", "--urlonly", action="store_true", default=False, dest="urlonly",
+        help="only print direct link to gif and exit"
+    )
 
     return parser.parse_args()
 
 def main():
     CLI = parseArgs()
-    downloadURL(CLI.URL)
+
+    downloadURL(
+        url=CLI.URL,
+        outLoc=CLI.output,
+        urlOnly=CLI.urlonly
+    )
